69bdbae9b5d9acb4329966cba6a54f8baff1b4ee
Henrik <henrik.cecilio@mail.uft.edu.br>
2021-08-09 13:51:17 -0300

    primeio commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new SimpleSlide({
+  slide: "servicos-slide", // nome do atributo data-slide="principal"
+  nav: true, // se deve ou não mostrar a navegação
+  auto: false, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
